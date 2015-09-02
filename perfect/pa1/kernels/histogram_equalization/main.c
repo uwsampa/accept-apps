e9ff5e8ccc126298a7d7d9b46395073f1ec96503
Thierry Moreau <moreau@uw.edu>
2015-09-02 14:32:43 -0400

    input and output file naming convention has to match autotuner

--- perfect/pa1/kernels/histogram_equalization/main.c
+++ perfect/pa1/kernels/histogram_equalization/main.c
@@ -102,7 +102,13 @@
 #define BATCH_SIZE (1)
 #endif
 
-#if INPUT_SIZE == INPUT_SIZE_SMALL
+#if INPUT_SIZE == AUTOTUNER
+#define M 640  /* columns */
+#define N 480  /* rows */
+#define FILENAME "input_small.mat"
+#define SIZE "small"
+
+#elif INPUT_SIZE == INPUT_SIZE_SMALL
 #define M 640  /* columns */
 #define N 480  /* rows */
 #define FILENAME "input_small.mat"
@@ -179,7 +185,11 @@ int main (int argc, char * argv[])
   for (i = 0; i < BATCH_SIZE; i++) // ACCEPT_FORBID
   {
     char buffer [30];
-    sprintf (buffer, "histeq_output.%d.mat", i);
+    #if INPUT_SIZE == AUTOTUNER
+      sprintf (buffer, "out.mat");
+    #else
+      sprintf (buffer, "histeq_output.%d.mat", i);
+    #endif
     write_array_to_octave (ENDORSE(&output[i * M * N]), N, M, buffer, "output_" SIZE);
   }
   PRINT_STAT_STRING ("output_file", "histeq_output." SIZE ".#.mat");
