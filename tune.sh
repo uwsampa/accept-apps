9b16a0a83c3d2b718fff4b7f0f9942b6f29c6320
Thierry Moreau <moreau@uw.edu>
2015-09-24 16:23:14 -0400

    tuning script for all benchmarks

---
+++ tune.sh
@@ -0,0 +1,33 @@
+#2d-conv benchmark
+(cd perfect/pa1/kernels/2d_convolution/; make tune TUNER_ARGS="-d -fixedrate -t 0.1 -c 8")
+#DWT53 benchmark
+(cd perfect/pa1/kernels/dwt53/; make tune TUNER_ARGS="-d -fixedrate -t 0.1 -c 28")
+#Histogram-eq benchmark
+(cd perfect/pa1/kernels/histogram_equalization/; make tune TUNER_ARGS="-d -fixedrate -t 0.1 -c 18")
+
+#FFT-1D benchmark
+(cd perfect/required/kernels/fft-1d/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 42")
+#FFT-2D benchmark
+(cd perfect/required/kernels/fft-2d/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 51")
+
+#BP benchmark
+(cd perfect/sar/kernels/bp/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 23")
+#PFA-interp1 benchmark
+(cd perfect/sar/kernels/pfa-interp1/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 23")
+#PFA-interp2 benchmark
+(cd perfect/sar/kernels/pfa-interp2/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 31")
+
+#Inner-product benchmark
+(cd perfect/stap/kernels/inner-product/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 25")
+#Outer-product benchmark
+(cd perfect/stap/kernels/outer-product/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 46")
+#System-solve benchmark
+(cd perfect/stap/kernels/system-solve/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 54")
+
+#Change-detection benchmark
+(cd perfect/wami/kernels/change-detection/; make tune TUNER_ARGS="-d -fixedrate -t 0.1 -c 29")
+#Debayer benchmark
+(cd perfect/wami/kernels/debayer/; make tune TUNER_ARGS="-d -fixedrate -t 0.1 -c 65")
+#Lucas-Kanade benchmark
+(cd perfect/wami/kernels/lucas-kanade/; make tune TUNER_ARGS="-d -fixedrate -snr 100.0 -c 50")
+
