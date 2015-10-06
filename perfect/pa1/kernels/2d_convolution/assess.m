c3bfc8c89cb82a07f961714d144b1427d11cbae3
Thierry Moreau <moreau@uw.edu>
2015-10-05 21:05:54 -0400

    evaluating 2d conv output degradation in terms of SNR

--- perfect/pa1/kernels/2d_convolution/assess.m
+++ perfect/pa1/kernels/2d_convolution/assess.m
@@ -1,53 +1,18 @@
-batch_size = 1;
-
-err = 0.0;
-combined_err = 0.0;
-
 arg_list = argv();
 
 if (length(arg_list)==2)
   orig_fn = arg_list{1};
   relax_fn = arg_list{2};
 
-  load(orig_fn)
-  load(relax_fn)
-  diff = abs(output_small - output_reference);
-  err_total = sum(sum(diff));
-  err = (err_total / (255.0)) / (rows(output_reference) * columns(output_reference));
+  load(orig_fn);
+  load(relax_fn);
 
-  fprintf(stdout, '%.16f\n', err);
-
+  if (norm(output_reference-output_small)==0)
+    fprintf('1.0e+9\n');
+  else
+    snr = 20 * log10( norm(output_reference) / norm(output_reference-output_small) );
+    fprintf(stdout, '%.2f\n', snr);
+  endif
 else
-
-  for i = 0:(batch_size - 1)
-    load(["2dconv_output." (int2str(i)) ".mat"])
-    if (exist("output_small", "var"))
-      ## disp("small")
-      load 2dconv_output.small.mat
-      diff = abs(output_small - output_reference);
-      err_total = sum(sum(diff));
-      err = (err_total / 255.0) / (rows(output_reference) * columns(output_reference));
-    elseif (exist("output_medium", "var"))
-      ## disp("medium")
-      load 2dconv_output.medium.mat
-      diff = abs(output_medium - output_reference);
-      err_total = sum(sum(diff));
-      err = (err_total / 255.0) / (rows(output_reference) * columns(output_reference));
-    elseif (exist("output_large", "var"))
-      ## disp("large")
-      load 2dconv_output.large.mat
-      diff = abs(output_large - output_reference);
-      err_total = sum(sum(diff));
-      err = (err_total / 255.0) / (rows(output_reference) * columns(output_reference));
-    else
-      ## disp("unknown")
-      err = 1.0;
-    endif
-
-    combined_err = combined_err + err;
-
-  endfor
-
-  fprintf(stdout, '%.16f\n', combined_err / batch_size);
-
+  fprintf('1.0\n');
 endif
