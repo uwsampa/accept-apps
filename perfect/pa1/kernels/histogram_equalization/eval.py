84e749800e204aeaac0f0d83cc032b7bf606d5c5
Thierry Moreau <moreau@uw.edu>
2016-01-31 17:12:53 -0500

    updated PA1 quality metric implementation

--- perfect/pa1/kernels/histogram_equalization/eval.py
+++ perfect/pa1/kernels/histogram_equalization/eval.py
@@ -1,18 +1,15 @@
-from subprocess import check_output
 import os
+import sys
+sys.path.append('../../../utils/')
+import perfectlib
 
 EXT = ".mat"
 
-def load():
-    output = check_output(["octave", "-q", "assess.m"])
-    return output
-
-# assess.m is an octave script that computes a signal to noise ratio in dB
-# by comparing a reference hist_ew kernel output to an approximate output.
-
-
 def score(orig, relaxed):
     if (os.path.isfile(relaxed)):
-        return float(check_output(["octave", "-q", "assess.m", orig, relaxed]))
+        return perfectlib.computeSNR(orig, relaxed, "mat")
     else:
         return 1.0
+
+if __name__ == '__main__':
+    print score('orig'+EXT, 'out'+EXT)
