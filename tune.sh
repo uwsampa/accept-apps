d8eaaf0d441100fc342aecd51b647620b28cf32d
Thierry Moreau <moreau@uw.edu>
2016-07-22 15:57:35 -0700

    adjusting cluster parallelization factor

--- tune.sh
+++ tune.sh
@@ -8,44 +8,44 @@ mv perfect/pa1/kernels/2d_convolution/outputs runs/pa1_2d_convolution
 (cd perfect/pa1/kernels/dwt53/; make tune TUNER_ARGS="-d -snr 10.0 -c 54")
 mv perfect/pa1/kernels/dwt53/outputs runs/pa1_dwt53
 #Histogram-eq benchmark
-(cd perfect/pa1/kernels/histogram_equalization/; make tune TUNER_ARGS="-d -snr 10.0 -c 16")
+(cd perfect/pa1/kernels/histogram_equalization/; make tune TUNER_ARGS="-d -snr 10.0 -c 20")
 mv perfect/pa1/kernels/histogram_equalization/outputs runs/pa1_histogram_equalization
 
 #Debayer benchmark
-(cd perfect/wami/kernels/debayer/; make tune TUNER_ARGS="-d -snr 10.0 -c 84")
+(cd perfect/wami/kernels/debayer/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/wami/kernels/debayer/outputs runs/wami_debayer
 #Lucas-Kanade benchmark
-(cd perfect/wami/kernels/lucas-kanade/; make tune TUNER_ARGS="-d -snr 10.0 -c 93")
+(cd perfect/wami/kernels/lucas-kanade/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/wami/kernels/lucas-kanade/outputs runs/wami_lucas-kanade
 #Change-detection benchmark
-(cd perfect/wami/kernels/change-detection/; make tune TUNER_ARGS="-d -snr 10.0 -c 70")
+(cd perfect/wami/kernels/change-detection/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/wami/kernels/change-detection/outputs runs/wami_change-detection
 
 #Outer-product benchmark
-(cd perfect/stap/kernels/outer-product/; make tune TUNER_ARGS="-d -snr 10.0 -c 71")
+(cd perfect/stap/kernels/outer-product/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/stap/kernels/outer-product/outputs runs/stap_outer-product
 #System-solve benchmark
-(cd perfect/stap/kernels/system-solve/; make tune TUNER_ARGS="-d -snr 10.0 -c 77")
+(cd perfect/stap/kernels/system-solve/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/stap/kernels/system-solve/outputs runs/stap_system-solve
 #Inner-product benchmark
-(cd perfect/stap/kernels/inner-product/; make tune TUNER_ARGS="-d -snr 10.0 -c 83")
+(cd perfect/stap/kernels/inner-product/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/stap/kernels/inner-product/outputs runs/stap_inner-product
 
 #FFT-1D benchmark
-(cd perfect/required/kernels/fft-1d/; make tune TUNER_ARGS="-d -snr 10.0 -c 46")
+(cd perfect/required/kernels/fft-1d/; make tune TUNER_ARGS="-d -snr 10.0 -c 85")
 mv perfect/required/kernels/fft-1d/outputs runs/required_fft-1d
 #FFT-2D benchmark
 (cd perfect/required/kernels/fft-2d/; make tune TUNER_ARGS="-d -snr 10.0 -c 100")
 mv perfect/required/kernels/fft-2d/outputs runs/required_fft-2d
 
 #PFA-interp1 benchmark
-(cd perfect/sar/kernels/pfa-interp1/; make tune TUNER_ARGS="-d -snr 10.0 -c 40")
+(cd perfect/sar/kernels/pfa-interp1/; make tune TUNER_ARGS="-d -snr 10.0 -c 53")
 mv perfect/sar/kernels/pfa-interp1/outputs runs/sar_pfa-interp1
 #PFA-interp2 benchmark
-(cd perfect/sar/kernels/pfa-interp2/; make tune TUNER_ARGS="-d -snr 10.0 -c 40")
+(cd perfect/sar/kernels/pfa-interp2/; make tune TUNER_ARGS="-d -snr 10.0 -c 52")
 mv perfect/sar/kernels/pfa-interp2/outputs runs/sar_pfa-interp2
 #BP benchmark
-(cd perfect/sar/kernels/bp/; make tune TUNER_ARGS="-d -snr 10.0 -c 43")
+(cd perfect/sar/kernels/bp/; make tune TUNER_ARGS="-d -snr 10.0 -c 56")
 mv perfect/sar/kernels/bp/outputs runs/sar_bp
 
 
